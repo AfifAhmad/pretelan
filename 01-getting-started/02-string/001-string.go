be2f49f87d24b68e3b97e5b3f8b9abab8895ce89
unknown <superteenager@gmail.com>
2023-08-21 21:31:26 +0700

    string

---
+++ 01-getting-started/02-string/001-string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("");
+    fmt.Println("A");
+    fmt.Println("Huehue");
+    fmt.Println("Abraham Lincoln");
+}
+
+/* 
+
+String merupakan tipe data yang berupa kumpulan karakter untuk menyimpan data seperti kata-kata, kalimat, paragraf, atau nama. Panjangnya bisa 0, atau 1 atau lebih
+Tulisan berupa Abraham Lincoln dan Huehue merupakan contoh dari string
+*/
---
+++ 01-getting-started/02-string/002-string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Semarang"+" Semakin Hebat");
+    fmt.Println(""+"Bandarharjo Jaya");
+    fmt.Println("1"+"1");
+}
+
+/* 
+Contoh di video ini adalah penggabungan string
+Yaitu dengan tanda + diikuti string yang akan ditambahkan
+Jika anda ingin mendapatkan hasil 1 + 1 adalah 11 
+Anda bisa menggunakan string
+*/
